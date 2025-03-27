dddfec41d9c2897f30ec851c47af46a2697c7c7e
flash659 <57206771+Cafelatte1@users.noreply.github.com>
2025-03-27 14:08:56 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="frony-document-processor",
-    version="0.1.0",
+    version="0.2.0",
     packages=["frony_document_processor"],
     install_requires=[
         "numpy",
